2623c993bf3b2ca01b48696ff8910013772ee556
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-17 15:22:27 +0530

    Greatest of 3 integers

--- src/Generics.java
+++ src/Generics.java
@@ -1,5 +1,27 @@
 public class Generics {
+    public void PrintMax(int a, int b, int c){
+        if (a>b){
+            if (a>c){
+                System.out.println(a +" is max.");
+            } else {
+                System.out.println(c +" is max.");
+            }
+        } else {
+            if (b>c){
+                System.out.println(b +" is max.");
+            } else {
+                System.out.println(c +" is max.");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to generics program");
+        Generics max = new Generics();
+        //TestCase1
+        max.PrintMax(2,3,4);
+        //TestCase2
+        max.PrintMax(2,4,3);
+        //TestCase3
+        max.PrintMax(4,2,3);
     }
 }
