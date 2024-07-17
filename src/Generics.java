27591820a0e1b555884237d9f5bbcbf414250f67
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-17 15:56:47 +0530

    Getting any number of parameters

--- src/Generics.java
+++ src/Generics.java
@@ -1,35 +1,40 @@
 public class Generics<E extends Comparable<E>> {
-    private final E a;
-    private final E b;
-    private final E c;
 
-    public Generics(E a, E b, E c){
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    private final E[] val;
+
+    @SafeVarargs
+    public Generics(E... val){
+        this.val = val;
     }
 
-    public void PrintMax(){
-        if (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) {
-            System.out.println(a + " is max.");
-        } else if (b.compareTo(a) >= 0 && b.compareTo(c) >= 0) {
-            System.out.println(b + " is max.");
-        } else {
-            System.out.println(c + " is max.");
+    public E findMaximum() {
+        if (val == null || val.length == 0) {
+            throw new IllegalArgumentException("Array must not be empty");
         }
+
+        E max = val[0];
+        for (int i = 1; i < val.length; i++) {
+            if (val[i].compareTo(max) > 0) {
+                max = val[i];
+            }
+        }
+        return max;
+    }
+
+    public void printMax() {
+        E max = findMaximum();
+        System.out.println("Maximum value is " + max);
     }
 
     public static void main(String[] args) {
         //Testcase1
-        Generics<Integer> maxInt = new Generics<>(2, 3,4);
-        maxInt.PrintMax();
-
+        Generics<Integer> maxInt = new Generics<>(10, 5, 8, 15, 3);
+        maxInt.printMax();
         //Testcase2
-        Generics<Float> maxFloat = new Generics<>(2.5f,4.2f,3.7f);
-        maxFloat.PrintMax();
-
+        Generics<Float> maxFloat = new Generics<>(3.5f, 7.2f, 4.9f, 6.1f);
+        maxFloat.printMax();
         //Testcase3
-        Generics<String> maxString = new Generics<>("Banana", "Peach", "Apple");
-        maxString.PrintMax();
+        Generics<String> maxString = new Generics<>("Apple", "Peach", "Banana", "Orange");
+        maxString.printMax();
     }
 }
