1c35ef69b3a69fe6bd8708304316a800e22c13da
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-17 15:42:34 +0530

    Using Generic Methord

--- src/Generics.java
+++ src/Generics.java
@@ -1,26 +1,20 @@
 public class Generics {
-    public void PrintMax(String a, String b, String c){
-        if (a.length()>b.length()){
-            if (a.length()>c.length()){
-                System.out.println(a +" is max.");
-            } else {
-                System.out.println(c +" is max.");
-            }
+    public <E extends Comparable<E>> void PrintMax(E a, E b, E c){
+        if (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) {
+            System.out.println(a + " is max.");
+        } else if (b.compareTo(a) >= 0 && b.compareTo(c) >= 0) {
+            System.out.println(b + " is max.");
         } else {
-            if (b.length()>c.length()){
-                System.out.println(b +" is max.");
-            } else {
-                System.out.println(c +" is max.");
-            }
+            System.out.println(c + " is max.");
         }
     }
 
     public static void main(String[] args) {
         Generics max = new Generics();
         //TestCase1
-        max.PrintMax("Apple", "Peach", "Banana");
+        max.PrintMax(2,3,4);
         //TestCase2
-        max.PrintMax("Apple", "Banana", "Peach");
+        max.PrintMax(2.5f,4.2f,3.7f);
         //TestCase3
         max.PrintMax("Banana", "Peach", "Apple");
     }
