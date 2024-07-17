eab9168e7b236237939e7e149544c5d746ab03ec
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-17 17:09:36 +0530

    Used generic methord to find maximum

--- src/Generics.java
+++ src/Generics.java
@@ -3,37 +3,38 @@ public class Generics<E extends Comparable<E>> {
     private final E[] val;
 
     @SafeVarargs
-    public Generics(E... val){
+    public Generics(E... val) {
         this.val = val;
     }
 
-    public E findMaximum() {
-        if (val == null || val.length == 0) {
+    @SafeVarargs
+    public static <E extends Comparable<E>> E findMaximum(E... values) {
+        if (values == null || values.length == 0) {
             throw new IllegalArgumentException("Array must not be empty");
         }
 
-        E max = val[0];
-        for (int i = 1; i < val.length; i++) {
-            if (val[i].compareTo(max) > 0) {
-                max = val[i];
+        E max = values[0];
+        for (int i = 1; i < values.length; i++) {
+            if (values[i].compareTo(max) > 0) {
+                max = values[i];
             }
         }
         return max;
     }
 
     public void printMax() {
-        E max = findMaximum();
+        E max = findMaximum(val);
         System.out.println("Maximum value is " + max);
     }
 
     public static void main(String[] args) {
-        //Testcase1
+        // Testcase1
         Generics<Integer> maxInt = new Generics<>(10, 5, 8, 15, 3);
         maxInt.printMax();
-        //Testcase2
+        // Testcase2
         Generics<Float> maxFloat = new Generics<>(3.5f, 7.2f, 4.9f, 6.1f);
         maxFloat.printMax();
-        //Testcase3
+        // Testcase3
         Generics<String> maxString = new Generics<>("Apple", "Peach", "Banana", "Orange");
         maxString.printMax();
     }
