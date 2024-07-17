96351e73d385694dc312fb52525a63f69f666cc2
ksr12345 <kaustubh.raut@somaiya.edu>
2024-07-17 15:50:02 +0530

    Using Generic Class

--- src/Generics.java
+++ src/Generics.java
@@ -1,5 +1,15 @@
-public class Generics {
-    public <E extends Comparable<E>> void PrintMax(E a, E b, E c){
+public class Generics<E extends Comparable<E>> {
+    private final E a;
+    private final E b;
+    private final E c;
+
+    public Generics(E a, E b, E c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public void PrintMax(){
         if (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) {
             System.out.println(a + " is max.");
         } else if (b.compareTo(a) >= 0 && b.compareTo(c) >= 0) {
@@ -10,12 +20,16 @@ public class Generics {
     }
 
     public static void main(String[] args) {
-        Generics max = new Generics();
-        //TestCase1
-        max.PrintMax(2,3,4);
-        //TestCase2
-        max.PrintMax(2.5f,4.2f,3.7f);
-        //TestCase3
-        max.PrintMax("Banana", "Peach", "Apple");
+        //Testcase1
+        Generics<Integer> maxInt = new Generics<>(2, 3,4);
+        maxInt.PrintMax();
+
+        //Testcase2
+        Generics<Float> maxFloat = new Generics<>(2.5f,4.2f,3.7f);
+        maxFloat.PrintMax();
+
+        //Testcase3
+        Generics<String> maxString = new Generics<>("Banana", "Peach", "Apple");
+        maxString.PrintMax();
     }
 }
